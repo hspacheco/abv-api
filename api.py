82d1ac0b209c6b2b8bf8dd86e1b82f78f55ec852
hspacheco <henrique.pacheco@zigpay.com.br>
2022-06-24 22:17:59 -0300

    try

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 from flask_cors import CORS
-from firebase import download_file, download_all_files, get_all_files
-from lsa_process import get_LSA, clear_files
-import os
+# from firebase import download_file, download_all_files, get_all_files
+# from lsa_process import get_LSA, clear_files
+# import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -230,31 +230,30 @@ SYMPTOMS = [
     }
 ]
 
-
 class SymptomsList(Resource):
     def get(self):
        return SYMPTOMS, 200
 
-class FileNames(Resource):
-    def get(self):
-        return get_all_files()
+# class FileNames(Resource):
+#     def get(self):
+#         return get_all_files()
 
-class LsaScore(Resource):
-    def get(self):
-      parser = reqparse.RequestParser()
-      parser.add_argument('symptom', action='append', location='args')
-      args = parser.parse_args()
+# class LsaScore(Resource):
+#     def get(self):
+#       parser = reqparse.RequestParser()
+#       parser.add_argument('symptom', action='append', location='args')
+#       args = parser.parse_args()
 
-      # download_all_files()
-      # lsa_result = get_LSA()
-      # clear_files()
-      return args['symptom'], 200
+#       # download_all_files()
+#       # lsa_result = get_LSA()
+#       # clear_files()
+#       return args['symptom'], 200
 
 
 
 api.add_resource(SymptomsList, '/symptoms')
-api.add_resource(FileNames, '/file-names')
-api.add_resource(LsaScore, '/lsa-score')
+# api.add_resource(FileNames, '/file-names')
+# api.add_resource(LsaScore, '/lsa-score')
 
 if __name__ == '__main__':
     app.run(debug=True)
